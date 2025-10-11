cf998372b7321b9cac713fc86ab3541830c3cff9
Rafipilot <Rafayel.latif@gmail.com>
2025-10-11 10:25:05 +0100

    Detection of possible passwords plus code cleanup

--- content.js
+++ content.js
@@ -1,13 +1,45 @@
 // this script is going to be listing to user keystrokes to detect if they type any sensitive info so we can warn them!
 
-const keywords = ["password", "ssn", "credit card", "address", "phone number", "email"]
-document.addEventListener("input", (event)=>{
-    const target = event.target
-    console.log("User typed: ", target.value)
-    keywords.forEach(keyword => {
-        if (target.value.includes(keyword)) {
-            alert(`Warning: You typed a sensitive keyword - ${keyword}`)
-        }
-    })
-})
+function checkIfPassword(typed)  {
+    // Rules: if string is more than 8 chars no spaces, includes numbers and capitals then flag
 
+    for (const [keyword, value] of Object.entries(keywords)) {
+        if (typed.includes(value)) {
+            alert(`Warning: You typed a sensitive keyword - ${keyword}`)
+            return true
+        }
+    }
+
+    different_words = typed.split(" ")
+    for (const word of different_words) {
+        if (/\d/.test(word)) {
+            if (/[A-Z]/.test(word)) {
+                alert("You may have typed a sensitive password - "+ word)
+                return true
+            }
+        }
+    }
+
+    return false
+}
+
+previously_typed = ""
+document.addEventListener("keydown", (event) => {
+    console.log(`Key "${event.key}" pressed  [event: keydown]`);
+    if (event.key.length > 1) {
+        if (event.key = "Backspace")    {
+            previously_typed = previously_typed.slice(0, -1)
+        }
+        return; // ignore special keys like Shift, Ctrl, etc.
+    }
+    previously_typed += event.key.toLowerCase()
+    if (previously_typed.length > 50) {
+        previously_typed = previously_typed.slice(-50) // keep only the last 50 characters
+    }
+    console.log(`Previously typed: ${previously_typed}`);
+
+    ispassword = checkIfPassword(previously_typed)
+    if (ispassword) {
+        previously_typed = "" // reset previously typed if we flag password
+    }
+});
